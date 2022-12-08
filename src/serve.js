06f52fa725a4dc5c5373eaa9a6451108989998f1
Jonas Tiago <jonastiago95@gmail.com>
2022-12-08 11:26:57 -0300

    feat: route post categories

--- src/serve.js
+++ src/serve.js
@@ -12,10 +12,19 @@ const connection = new Pool({
 });
 
 const app = express();
+app.use(express.json());
 
 app.get("/categories", async (req, res) => {
   const categories = await connection.query("SELECT * FROM categories;");
   res.send(categories.rows);
+});
+
+app.post("/categories", async (req, res) => {
+  const { name } = req.body;
+
+  await connection.query("INSERT INTO categories (name) VALUES ($1)", [name]);
+
+  res.sendStatus(201);
 });
 
 const port = process.env.PORT || 4000;
