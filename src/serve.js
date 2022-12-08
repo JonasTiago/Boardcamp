6d9d9c3c7d8f12dcf2dfe4cb6abe08d80d31403a
Jonas Tiago <jonastiago95@gmail.com>
2022-12-08 10:51:35 -0300

    feat: route get categories

--- src/serve.js
+++ src/serve.js
@@ -13,6 +13,10 @@ const connection = new Pool({
 
 const app = express();
 
+app.get("/categories", async (req, res) => {
+  const categories = await connection.query("SELECT * FROM categories;");
+  res.send(categories.rows);
+});
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server running in port ${port}`));
