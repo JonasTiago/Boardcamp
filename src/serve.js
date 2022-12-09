3f14915c3853d60d5fb242683d95ad8a8f2c5351
Jonas Tiago <jonastiago95@gmail.com>
2022-12-09 10:36:24 -0300

    feat: route get games

--- src/serve.js
+++ src/serve.js
@@ -43,5 +43,20 @@ app.get("/games", async (req, res) => {
   }
 });
 
+app.post("/games", async (req, res) => {
+  const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
+
+  try {
+    await connection.query(
+      'INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1,$2,$3,$4,$5);',
+      [name, image, stockTotal, categoryId, pricePerDay]
+    );
+    console.log("oi");
+    res.sendStatus(201);
+  } catch (err) {
+    res.sendStatus(500);
+  }
+});
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Server running in port ${port}`));
